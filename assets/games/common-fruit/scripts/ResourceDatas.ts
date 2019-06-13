a1c4cde19281747e1a2ae4a01a281a05769b7459
xuzhirui <286942689@qq.com>
2019-06-13 09:16:17 +0800

    学习模式场景增加图片的文本显示对象 资源数据增加语音数据的定义

--- assets/games/common-fruit/scripts/ResourceDatas.ts
+++ assets/games/common-fruit/scripts/ResourceDatas.ts
@@ -22,6 +22,24 @@ export default class NewClass extends cc.Component {
     @property(cc.SpriteFrame)
     bananaSprite: cc.SpriteFrame[] = [];
 
+    @property(cc.AudioClip)
+    pearAudio: cc.AudioClip[] = [];
+
+    @property(cc.AudioClip)
+    appleAudio: cc.AudioClip[] = [];
+
+    @property(cc.AudioClip)
+    grapeAudio: cc.AudioClip[] = [];
+
+    @property(cc.AudioClip)
+    orangeAudio: cc.AudioClip[] = [];
+
+    @property(cc.AudioClip)
+    watermelonAudio: cc.AudioClip[] = [];
+
+    @property(cc.AudioClip)
+    bananaAudio: cc.AudioClip[] = [];
+
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
