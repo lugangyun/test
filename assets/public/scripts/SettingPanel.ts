441911b5dc13ad585bcf3ccb7d350cbdba3d8849
xuzhirui <286942689@qq.com>
2019-06-19 14:36:10 +0800

    设置面板增加精灵帧属性默认值设置 显示面板完成动态计算节点位置，摆放文本节点和设置按钮节点

--- assets/public/scripts/SettingPanel.ts
+++ assets/public/scripts/SettingPanel.ts
@@ -4,40 +4,75 @@ const { ccclass, property } = cc._decorator;
 export default class SettingPanel extends cc.Component {
 
     @property(cc.SpriteFrame)
-    buttonNormal: cc.SpriteFrame;
+    buttonNormal: cc.SpriteFrame = null;
 
     @property(cc.SpriteFrame)
-    buttonDown: cc.SpriteFrame;
+    buttonDown: cc.SpriteFrame = null;
 
     async show(setting: any): Promise<any> {
+        console.log(this.buttonNormal)
         this.node.getComponent(cc.Animation).play("panelPopupAnimation");
         let content = this.node.getChildByName("content");
-        let lineNum = 0;
+        let lineHeight = content.height / Object.getOwnPropertyNames(setting).length;
+        let contentDist = 40;
+        let contentLineDist = 80;
+        let offsetX = 0, offsetY = this.contentY0 - contentDist;
         for (let key in setting) {
-            lineNum++;
             let labelNode = new cc.Node();
             labelNode.addComponent(cc.Label);
             labelNode.color = cc.color(0x10, 0x79, 0xa5);
-            labelNode.anchorX = labelNode.anchorY = 0;
+            labelNode.anchorX = 0;
             let label = labelNode.getComponent(cc.Label);
             label.fontSize = 40;
-            label.string = key;
+            label.string = key + "：";
+            labelNode.x = this.contentX0;
+            labelNode.y = offsetY;
             content.addChild(labelNode);
+
             let choices: string[] = setting[key];
+            let initX = labelNode.x + labelNode.width;
+            offsetX = initX;
             choices.forEach((choice, index) => {
+                let buttonNode = new cc.Node();
+                let sprite = buttonNode.addComponent(cc.Sprite);
+                sprite.type = cc.Sprite.Type.SLICED;
+                sprite.spriteFrame = this.buttonNormal;
+                buttonNode.addComponent(cc.Button);
+                let button = buttonNode.getComponent(cc.Button);
+                button.normalSprite = this.buttonNormal;
+                button.pressedSprite = this.buttonDown;
+                button.transition = cc.Button.Transition.SCALE;
+                button.duration = 0.1;
+                button.zoomScale = 0.8;
 
+                let buttonLabelNode = new cc.Node();
+                buttonLabelNode.color = cc.color(0, 177, 78);
+                let buttonLabel = buttonLabelNode.addComponent(cc.Label);
+                buttonLabel.string = choice;
+                buttonLabel.fontSize = 30;
+
+                buttonNode.width = Math.max(choice.length * buttonLabel.fontSize + 20, 100);
+                if (offsetX + buttonNode.width + contentDist > content.width / 2) {
+                    offsetX = initX;
+                    offsetY -= contentLineDist;
+                }
+                buttonNode.y = offsetY;
+                buttonNode.x = offsetX + buttonNode.width / 2;
+                offsetX = buttonNode.x + buttonNode.width / 2 + contentDist;
+
+                buttonNode.addChild(buttonLabelNode);
+                content.addChild(buttonNode);
             });
+            offsetY -= contentLineDist;
         }
-        console.log(content);
-
     }
 
-    private get x0() {
-        return -this.node.width / 2;
+    private get contentX0() {
+        return -this.node.getChildByName("content").width / 2;
     }
 
-    private get y0() {
-        return this.node.height / 2;
+    private get contentY0() {
+        return this.node.getChildByName("content").height / 2;
     }
 
     // LIFE-CYCLE CALLBACKS:
