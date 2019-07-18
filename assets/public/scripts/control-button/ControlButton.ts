97c733c78228bf4dc22c9302682e411b623c8e2a
lugangyun <lugangyun@163.com>
2019-07-18 18:54:16 +0800

    完成按钮内文本颜色根据按钮状态改变

--- assets/public/scripts/control-button/ControlButton.ts
+++ assets/public/scripts/control-button/ControlButton.ts
@@ -27,8 +27,8 @@ export default class ControlButton extends cc.Component {
     replay: cc.Node = null;
 
     get allNodes() {
-        return [this.playAll, this.backToPlayStep, this.redo, this.last, this.next, 
-            this.guide, this.replay];
+        return [this.playAll, this.backToPlayStep, this.redo, this.last, this.next,
+        this.guide, this.replay];
     }
 
     async clickEventHandle(event: cc.Event.EventTouch, args: string) {
@@ -41,6 +41,7 @@ export default class ControlButton extends cc.Component {
         this.allNodes.forEach((node) => {
             if (node) {
                 node.getComponent(cc.Button).interactable = false;
+                this.updateLabelColor(node);
             }
         });
     }
@@ -49,21 +50,59 @@ export default class ControlButton extends cc.Component {
         this.allNodes.forEach((node) => {
             if (node) {
                 node.getComponent(cc.Button).interactable = true;
+                this.updateLabelColor(node);
             }
         });
     }
 
     public setButtonState(type: string, enable: boolean) {
         this[type].getComponent(cc.Button).interactable = enable;
+        this.updateLabelColor(this[type]);
     }
 
     public setNodeActive(type: string, active: boolean) {
         this[type].active = active;
     }
 
+    private updateLabelColor(buttonNode: cc.Node) {
+        let buttonComponent = buttonNode.getComponent(cc.Button);
+        let labelNode = buttonNode.getChildByName("Background").getChildByName("Label");
+        if (!buttonComponent.interactable) {
+            labelNode.color = this.labelColor.disable;
+        }
+        else if (this.currentButton != buttonNode) {
+            labelNode.color = this.labelColor.normal;
+        }
+        else {
+            if (this.buttonState.down && this.buttonState.enter) {
+                labelNode.color = this.labelColor.down;
+            }
+            else if (this.buttonState.enter) {
+                labelNode.color = this.labelColor.enter;
+            }
+            else {
+                labelNode.color = this.labelColor.normal;
+            }
+        }
+    }
+
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
+
+    buttonState = {
+        enter: false,
+        down: false
+    }
+
+    currentButton: cc.Node;
+
+    labelColor = {
+        normal: cc.color(0x19, 0xae, 0x9b),
+        enter: cc.color(0xff, 0xa6, 0x33),
+        down: cc.color(0x23, 0x54, 0x18),
+        disable: cc.color(0x63, 0x63, 0x63)
+    }
 
     start() {
         for (let buttonNode of this.allNodes) {
@@ -73,7 +112,35 @@ export default class ControlButton extends cc.Component {
                 eventHandler.component = "ControlButton";
                 eventHandler.handler = "clickEventHandle";
                 eventHandler.customEventData = buttonNode.name;
-                buttonNode.getComponent(cc.Button).clickEvents.push(eventHandler);
+                let buttonComponent = buttonNode.getComponent(cc.Button);
+                buttonComponent.clickEvents.push(eventHandler);
+
+                let labelNode = buttonNode.getChildByName("Background").getChildByName("Label");
+                buttonNode.on(cc.Node.EventType.MOUSE_ENTER, (event: cc.Event) => {
+                    this.buttonState.enter = true;
+                    this.currentButton = event.target;
+                    this.updateLabelColor(buttonNode);
+                });
+                buttonNode.on(cc.Node.EventType.MOUSE_LEAVE, (event: cc.Event) => {
+                    this.buttonState.enter = false;
+                    this.currentButton = event.target;
+                    this.updateLabelColor(buttonNode);
+                });
+                buttonNode.on(cc.Node.EventType.TOUCH_START, (event: cc.Event) => {
+                    this.buttonState.down = true;
+                    this.currentButton = event.target;
+                    this.updateLabelColor(buttonNode);
+                });
+                buttonNode.on(cc.Node.EventType.TOUCH_END, (event: cc.Event) => {
+                    this.buttonState.down = false;
+                    this.currentButton = event.target;
+                    this.updateLabelColor(buttonNode);
+                });
+                buttonNode.on(cc.Node.EventType.TOUCH_CANCEL, (event: cc.Event) => {
+                    this.buttonState.down = false;
+                    this.currentButton = event.target;
+                    this.updateLabelColor(buttonNode);
+                });
             }
         }
     }
